8e3baa40dd9c846f5c9eecfa709b46208db25ec7
Bastian Noffer <bastian.noffer@gmail.com>
2025-03-18 19:40:57 +0100

    Initial import

---
+++ Shopware6.StoreApi/AppBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using Shopware6.StoreApi.Generated;
+
+namespace Shopware6.StoreApi;
+
+/// <summary>
+/// <see cref="IHostApplicationBuilder"/> Extensions
+/// </summary>
+public static class AppBuilderExtensions
+{
+    /// <summary>
+    /// Initializes the Shopware 6 Store API Client
+    /// </summary>
+    /// <param name="builder"><see cref="IHostApplicationBuilder"/></param>
+    /// <param name="baseUrl">BeseUrl of Shopware</param>
+    /// <returns><see cref="IHostApplicationBuilder"/> initialized for <see cref="Shopware6.StoreApi"/></returns>
+    public static IHostApplicationBuilder UseShopwareStoreApi(this IHostApplicationBuilder builder, string baseUrl)
+    {
+        var baseUri = new Uri(baseUrl);
+        builder.Services.ConfigureRefitClients(baseUrl: baseUri);
+        return builder;
+    }
+}
